a9b40d6bf8077b0d142c0ea63b158282bc4c387e
Francois Armand <francois.armand@normation.com>
2012-08-08 18:43:00 +0200

    Do not try to change password if the password file does not exist

--- Vagrant/server.sh
+++ Vagrant/server.sh
@@ -115,9 +115,11 @@ echo -e "\n192.168.42.90	node10.rudder.local" >> /etc/hosts
 echo '0,5,10,15,20,25,30,35,40,45,50,55 * * * * root if [ `ps -efww | grep cf-execd | grep "/var/rudder/cfengine-community/bin/cf-execd" | grep -v grep | wc -l` -eq 0 ]; then /var/rudder/cfengine-community/bin/cf-execd; fi' >> /etc/crontab
 
 # Set password to default passwords
-sed -i "s/\(RUDDER_WEBDAV_PASSWORD:\).*/\1rudder/" /opt/rudder/etc/rudder-passwords.conf
-sed -i "s/\(RUDDER_PSQL_PASSWORD:\).*/\1Normation/" /opt/rudder/etc/rudder-passwords.conf
-sed -i "s/\(RUDDER_OPENLDAP_BIND_PASSWORD:\).*/\1secret/" /opt/rudder/etc/rudder-passwords.conf
+if [ -e /opt/rudder/etc/rudder-passwords.conf]; then
+  sed -i "s/\(RUDDER_WEBDAV_PASSWORD:\).*/\1rudder/" /opt/rudder/etc/rudder-passwords.conf
+  sed -i "s/\(RUDDER_PSQL_PASSWORD:\).*/\1Normation/" /opt/rudder/etc/rudder-passwords.conf
+  sed -i "s/\(RUDDER_OPENLDAP_BIND_PASSWORD:\).*/\1secret/" /opt/rudder/etc/rudder-passwords.conf
+fi
 
 # Run cf-agent to set passwords to default
 /var/rudder/cfengine-community/bin/cf-agent
